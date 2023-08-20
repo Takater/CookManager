9f73410a76d3208329b5e03f06eaacb91697264e
Guilherme <guilherme.moret.ps@gmail.com>
2023-08-20 10:46:21 -0400

    Models minor setup

--- backend/cookmanager/models.py
+++ backend/cookmanager/models.py
@@ -1,15 +1,15 @@
 from typing import Any
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-class User(models.Model):
+class BaseUser(AbstractBaseUser):
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=11)
     name = models.TextField(max_length=40)
-    staff = models.BooleanField(default=False)
-
-    def to_staff(self):
-        self.staff = True
 
     def __init__(self, user_id):
         self.id = user_id
         return self
+    
+class StaffUser(BaseUser):
+    staff_id = models.CharField(unique=True)
